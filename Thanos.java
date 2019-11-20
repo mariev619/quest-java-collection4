6839b241010236181059dc8b569f23d3a3a730c7
Marie Vendran <marie.vendran@gmail.com>
2019-11-20 21:47:49 +0100

    first commit

--- Thanos.java
+++ Thanos.java
@@ -1,3 +1,8 @@
+import java.util.TreeMap;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Comparator;
+
 public class Thanos {
 
     public static void main(String[] args) {
@@ -21,10 +26,28 @@ public class Thanos {
         // Vision has a Tulip
         // Captain America has a Lily
         // Doctor Strange and Black Widow have a Violet
+        Comparator<Hero> comparator = new Comparator<Hero>() {
+            public int compare(Hero orig, Hero compare) {
+                return orig.getName().compareTo(compare.getName());
+            }
+        };
+        Map<Hero, Flower> treeThanos = new TreeMap<>(comparator);
+        treeThanos.put(hulk, rose);
+        treeThanos.put(thor, rose);
+        treeThanos.put(scarletWitch, rose);
+        treeThanos.put(vision, tulip);
+        treeThanos.put(captainAmerica, lily);
+        treeThanos.put(doctorStrange, violet);
+        treeThanos.put(blackWidow, violet);
 
         // TODO 2 : Print if `begonia` is contained in the TreeMap
+        Map<Hero, Flower> survivor = new HashMap<>();
+        System.out.println(survivor.containsKey(begonia));
 
         // TODO 3 : For each hero, alphabetically, print the corresponding flower
-
+        for (Hero hero : treeThanos.keySet()) {
+            Flower flower = treeThanos.get(hero);
+            System.out.println(hero.getName() + ": " + flower.getName());
+        }
     }
 }
